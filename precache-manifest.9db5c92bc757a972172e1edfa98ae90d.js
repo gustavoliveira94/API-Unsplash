514f398f491bfbf05d54e4f485dfc36898bbb064
Gustavo Oliveira <gusttavo@live.com>
2019-04-10 17:11:19 -0300

    Updates

---
+++ precache-manifest.9db5c92bc757a972172e1edfa98ae90d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0534ae1c3385743f89daa396d7a5877c",
+    "url": "/api-unsplash/static/media/bg.0534ae1c.jpg"
+  },
+  {
+    "revision": "64d7b47da16d9fe61cc6",
+    "url": "/api-unsplash/static/js/runtime~main.93908df9.js"
+  },
+  {
+    "revision": "bb12d1f0e50c8eb07a9b",
+    "url": "/api-unsplash/static/js/main.df2de4a3.chunk.js"
+  },
+  {
+    "revision": "2ad2653cd8a3f7cb3666",
+    "url": "/api-unsplash/static/js/2.89edce0c.chunk.js"
+  },
+  {
+    "revision": "bb12d1f0e50c8eb07a9b",
+    "url": "/api-unsplash/static/css/main.266485a1.chunk.css"
+  },
+  {
+    "revision": "66b779c9430f22eb7aff651ba634168a",
+    "url": "/api-unsplash/index.html"
+  }
+];
